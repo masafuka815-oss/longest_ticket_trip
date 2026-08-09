545dfb19aa8857b13086687acbe621f25c25cbcf
深町正聡 <masafuka815@gmail.com>
2026-08-09 12:21:35 +0900

    標準能力の読み込みとパース処理

---
+++ longest_path.py
@@ -0,0 +1,11 @@
+import sys
+
+for line in sys.stdin:
+	line = line.strip()
+	parts = line.split(",")
+
+	a = int(parts[0].strip())
+	b = int(parts[1].strip())
+	c = float(parts[2].strip())
+
+	print((a,b,c))
